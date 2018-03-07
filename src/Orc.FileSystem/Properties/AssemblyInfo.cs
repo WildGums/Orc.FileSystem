c05dc2d36a6dc31b73c748c3c3117ce87ac87489
Geert van Horrik <geert@catenalogic.com>
2018-03-07 23:49:27 +0100

    Migration to cake build

--- src/Orc.FileSystem/Properties/AssemblyInfo.cs
+++ src/Orc.FileSystem/Properties/AssemblyInfo.cs
@@ -6,14 +6,16 @@
 
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// All other assembly info is defined in SharedAssembly.cs
+// All other assembly info is defined in SolutionAssemblyInfo.cs
 
 [assembly: AssemblyTitle("Orc.FileSystem")]
 [assembly: AssemblyProduct("Orc.FileSystem")]
 [assembly: AssemblyDescription("Orc.FileSystem library")]
+[assembly: NeutralResourcesLanguage("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
