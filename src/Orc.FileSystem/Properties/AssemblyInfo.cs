0f00536ca5a69086b79a646758b305c172c32886
Geert van Horrik <geert@catenalogic.com>
2018-03-19 13:36:57 +0100

    Remove unneeded files

--- src/Orc.FileSystem/Properties/AssemblyInfo.cs
+++ src/Orc.FileSystem/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="WildGums">
-//   Copyright (c) 2008 - 2016 WildGums. All rights reserved.
+//   Copyright (c) 2008 - 2018 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -16,11 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Orc.FileSystem")]
 [assembly: AssemblyDescription("Orc.FileSystem library")]
 [assembly: NeutralResourcesLanguage("en-US")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("Orc.FileSystem.Tests")]
