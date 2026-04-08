798ad966828528d66738c1d600cca60b37b4f9e0
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:26:18 +0200

    Convert to file scoped namespaces

--- src/Orc.FileSystem/OrcFileSystemModule.cs
+++ src/Orc.FileSystem/OrcFileSystemModule.cs
@@ -1,25 +1,24 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Catel.ThirdPartyNotices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Orc.FileSystem;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcFileSystemModule
 {
-    using Catel.Services;
-    using Catel.ThirdPartyNotices;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Orc.FileSystem;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcFileSystemModule
+    public static IServiceCollection AddOrcFileSystem(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcFileSystem(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.TryAddSingleton<IFileService, FileService>();
-            serviceCollection.TryAddSingleton<IDirectoryService, DirectoryService>();
-            serviceCollection.TryAddSingleton<IIOSynchronizationService, IOSynchronizationService>();
+        serviceCollection.TryAddSingleton<IFileService, FileService>();
+        serviceCollection.TryAddSingleton<IDirectoryService, DirectoryService>();
+        serviceCollection.TryAddSingleton<IIOSynchronizationService, IOSynchronizationService>();
 
-            serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.FileSystem", "https://github.com/wildgums/orc.filesystem"));
+        serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.FileSystem", "https://github.com/wildgums/orc.filesystem"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
